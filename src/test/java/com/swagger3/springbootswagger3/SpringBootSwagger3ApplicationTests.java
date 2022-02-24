262d62b55f0fb16bfa6b3dffad05a15779018d65
leonardevans <lemutugi@gmail.com>
2022-02-24 02:42:39 -0500

    first commit

---
+++ src/test/java/com/swagger3/springbootswagger3/SpringBootSwagger3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagger3.springbootswagger3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSwagger3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
