ed7a2844c71610979b723e8c6b1c273f3da2e2cb
leonardevans <lemutugi@gmail.com>
2022-02-25 05:02:24 -0500

    EnableWebSecurity in SecurityConfiguration.java

---
+++ src/main/java/com/swagger3/springbootswagger3/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.swagger3.springbootswagger3.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/swagger-ui/**", "/my-openapi/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("javainuse")
+                .password(passwordEncoder().encode("my-openapi"))
+                .authorities("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
